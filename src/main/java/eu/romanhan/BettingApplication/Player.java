4ae80bb897489dfd6de8a839ec81042701b26bcb
Roman.Hanmamedov <Roman.Hanmamedov@EST-111.EST.local>
2024-03-13 12:51:59 +0200

    Added tests for Player class and made minor changes to Player and Match classes.

--- src/main/java/eu/romanhan/BettingApplication/Player.java
+++ src/main/java/eu/romanhan/BettingApplication/Player.java
@@ -6,21 +6,25 @@ import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Player {
 
 	private UUID playerId;
 	private long balance;
 	private int totalBets;
 	private int totalWins;
+	private boolean hasIllegalAction = false;
 
 	public void deposit(long amount) {
 		if (amount < 0) {
-			throw new IllegalArgumentException("Cannot deposit a negative amount");
+			hasIllegalAction = true;
+			throw new IllegalStateException("Illegal action! Cannot deposit a negative amount");
 		} else {
 			balance += amount;
 		}
@@ -28,7 +32,8 @@ public class Player {
 
 	public void withdraw(long amount) {
 		if (amount > balance) {
-			throw new IllegalStateException("Cannot withdrsw more than balance");
+			hasIllegalAction = true;
+			throw new IllegalStateException("Illegal action! Cannot withdrsw more than balance");
 		} else {
 			balance -= amount;
 		}
@@ -36,7 +41,8 @@ public class Player {
 
 	public void placeBet(Match match, String side, long amount) {
 		if (balance < amount) {
-			throw new IllegalStateException("Insufficient funds for bet");
+			hasIllegalAction = true;
+			throw new IllegalStateException("Illegal action! Insufficient funds for bet");
 		}
 		balance -= amount;
 
@@ -51,7 +57,7 @@ public class Player {
 	}
 
 	public BigDecimal calculateWinRate() {
-		if (totalBets == 0) {
+		if (totalBets == 0 || totalWins == 0) {
 			return new BigDecimal(0.0);
 		}
 		return BigDecimal.valueOf((double) totalWins / totalBets).setScale(2, RoundingMode.HALF_UP);
---
+++ src/test/java/eu/romanhan/BettingApplication/PlayerTest.java
@@ -0,0 +1,93 @@
+package eu.romanhan.BettingApplication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void testDeposit() {
+		Player player = new Player();
+		player.deposit(50);
+
+		assertEquals(50, player.getBalance());
+	}
+
+	@Test
+	void testDepositIllegalAmount() {
+		Player player = new Player();
+
+		assertThrows(IllegalStateException.class, () -> player.deposit(-50),
+				"Cannot deposit a negative amount message expected");
+		assertEquals(true, player.isHasIllegalAction());
+	}
+
+	@Test
+	void testWithdraw() {
+		Player player = new Player();
+		player.deposit(50);
+		player.withdraw(25);
+
+		assertEquals(25, player.getBalance());
+	}
+
+	@Test
+	void testWithdrawWhenInsufficientFunds() {
+		Player player = new Player();
+		player.deposit(50);
+
+		assertThrows(IllegalStateException.class, () -> player.withdraw(60), "Insufficient funds message expected");
+		assertEquals(true, player.isHasIllegalAction());
+	}
+
+	@Test
+	void testPlaceBet() {
+		Match match = new Match();
+		match.setResult("A");
+
+		Player player = new Player();
+		player.setTotalBets(5);
+		player.setTotalWins(1);
+
+		player.setBalance(50);
+		player.placeBet(match, "A", 10);
+
+		assertEquals(6, player.getTotalBets());
+		assertEquals(40, player.getBalance());
+		assertEquals(2, player.getTotalWins());
+	}
+
+	@Test
+	void testPlaceBetIllegalAction() {
+		Match match = new Match();
+		Player player = new Player();
+		player.setBalance(50);
+
+		assertThrows(IllegalStateException.class, () -> player.placeBet(match, "A", 60));
+	}
+
+	@Test
+	void testCalculateWinRate() {
+		Player player = new Player();
+		player.setTotalBets(2);
+		player.setTotalWins(5);
+		BigDecimal winRate = player.calculateWinRate();
+
+		assertEquals(BigDecimal.valueOf(2.50).setScale(winRate.scale()), winRate);
+	}
+
+	@Test
+	void testCalculateWinRateIfBetsOrWinsIsZero() {
+		Player player = new Player();
+		player.setTotalBets(0);
+		player.setTotalWins(0);
+		BigDecimal winRate = player.calculateWinRate();
+
+		assertEquals(BigDecimal.valueOf(0.00).setScale(winRate.scale()), winRate);
+	}
+
+}
